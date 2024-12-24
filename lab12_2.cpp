c950d90c51475113476de06b38b0c1e620598819
Anasuki <anatsapong_luechai@cmu.ac.th>
2024-12-24 15:38:18 +0700

    complete

--- lab12_2.cpp
+++ lab12_2.cpp
@@ -1,26 +1,19 @@
-#include <iostream>
-#include <ctime> 
+#include<iostream>
 using namespace std;
 
-
-int fibonacci(int x);
-
+int gcd(int x, int y);
 
 int main(){
-    clock_t start = clock();
-    cout << "Result: " << fibonacci(50) << "\n";
-    clock_t end = clock();
-    double elapsed = double(end - start)/CLOCKS_PER_SEC;
-    cout << "Elapsed Time: " << elapsed << " seconds.";
-    return 0;
+    int x, y;
+    cin >> x >> y;
+    cout << gcd(x,y);
+    return 0; 
 }
 
-int fibonacci(int x) {
-    if (x == 0) {
-        return 0; 
-    } else if (x == 1) {
-        return 1;
+int gcd(int x, int y) {
+    if (y == 0) {
+        return x;  
     } else {
-        return fibonacci(x - 1) + fibonacci(x - 2); 
+        return gcd(y, x % y);  
     }
 }
